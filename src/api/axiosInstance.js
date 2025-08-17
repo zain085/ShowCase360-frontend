fa87772ae3ed1975b8d9705545fcb7d0d432d2b4
Zain Ahmed <zainahmedkhan085@gmail.com>
2025-08-17 17:52:36 +0500

    add netlify route

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://showcase360-backend-production.up.railway.app',
+  baseURL: 'http://localhost:3000',
   withCredentials: true, 
   headers: {
     'Content-Type': 'application/json'
