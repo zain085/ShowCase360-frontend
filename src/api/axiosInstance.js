5626a02ee78981b43b4663747fa15ca953552d5c
Zain Ahmed <zainahmedkhan085@gmail.com>
2025-08-17 17:15:49 +0500

    edit baseurl

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'hshowcase360-backend-production.up.railway.app',
   withCredentials: true, 
   headers: {
     'Content-Type': 'application/json'
