1b007f79d6cefeeea314c58cfac0ee3eaadf65ff
Zain Ahmed <zainahmedkhan085@gmail.com>
2025-08-25 17:29:37 +0500

    added .env

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const BASE_URL = import.meta.env.VITE_API_URL;
+
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: BASE_URL,
   withCredentials: true, 
   headers: {
     'Content-Type': 'application/json'
